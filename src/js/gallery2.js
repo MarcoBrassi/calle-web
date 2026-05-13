4d4b71b7022783615079dc23375152abffaed553
Lydia <aguadoortegal@digitechfp.com>
2026-05-13 09:28:43 +0200

    proyecto Calle

---
+++ src/js/gallery2.js
@@ -0,0 +1,50 @@
+export const gallery2Items = [
+    {
+        type: "video",
+        src: "/media/video1.mp4",
+        title: "",
+        size: "large",
+    },  
+    {
+        type: "image",
+        src: "/media/img4.jpg",
+        title: "Proyecto 1",
+        size: "large",
+    },
+    {
+        type: "image",
+        src: "/media/img1.jpg",
+        title: "Proyecto 1",
+        size: "small",
+    },
+    {
+        type: "image",
+        src: "/media/img2.jpg",
+        title: "Proyecto 1",
+        size: "small",
+    },
+    {
+        type: "image",
+        src: "/media/img4.jpg",
+        title: "Proyecto 1",
+        size: "large",
+    },
+    {
+        type: "image",
+        src: "/media/img4.jpg",
+        title: "Proyecto 1",
+        size: "large",
+    },
+    {
+        type: "image",
+        src: "/media/img1.jpg",
+        title: "Proyecto 1",
+        size: "small",
+    },
+    {
+        type: "image",
+        src: "/media/img2.jpg",
+        title: "Proyecto 1",
+        size: "small",
+    },
+];
