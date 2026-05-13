677cee61073f5e13e19ca3e07cc03652f07dc67f
Lydia <aguadoortegal@digitechfp.com>
2026-05-13 13:55:13 +0200

    correción gallery2: ajustes en el grid / corrección header: ajustes en animación y sticky

--- src/js/gallery2.js
+++ src/js/gallery2.js
@@ -5,7 +5,7 @@ export const gallery2Items = [
         type: "image",
         src: "/media/images/webp/AndreaCalle_2.webp",
         title: "",
-        size: "large",
+        size: "medium",
     },
     {
         type: "image",
@@ -15,13 +15,13 @@ export const gallery2Items = [
     },
     {
         type: "image",
-        src: "/media/images/webp/AndreaCalle_3.webp",
+        src: "/media/images/webp/AndreaCalle_39.webp",
         title: "",
         size: "small",
     },
     {
         type: "image",
-        src: "/media/images/webp/AndreaCalle_13.webp",
+        src: "/media/images/webp/AndreaCalle_41.webp",
         title: "",
         size: "small",
     },
@@ -29,11 +29,11 @@ export const gallery2Items = [
         type: "video",
         src: "/media/video1.mp4",
         title: "",
-        size: "large",
+        size: "medium",
     }, 
     {
         type: "image",
-        src: "/media/images/webp/AndreaCalle_49.webp",
+        src: "/media/images/webp/AndreaCalle_136.webp",
         title: "",
         size: "large",
     },
