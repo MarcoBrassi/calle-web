3816c1cdad5b2113464ece165dc8073edafcef2f
Lydia <aguadoortegal@digitechfp.com>
2026-05-14 13:24:38 +0200

    reestructuración de carpetas

--- src/js/gallery1.js
+++ src/js/gallery1.js
@@ -1,36 +1,36 @@
 export const galleryItems = [
-    {
-        image: "/media/img1.jpg",
-        title: "Pretty Ballerinas",
-        desc: "Lasserie BParis SS24"
-    },
+    // {
+    //     image: "/media/img1.jpg",
+    //     title: "Pretty Ballerinas",
+    //     desc: "Lasserie BParis SS24"
+    // },
 
-    {
-        image: "/media/img2.jpg",
-        title: "Collet 23",
-        desc: "Lasserie BParis SS24"
-    },
+    // {
+    //     image: "/media/img2.jpg",
+    //     title: "Collet 23",
+    //     desc: "Lasserie BParis SS24"
+    // },
 
-    {
-        image: "/media/img3.jpg",
-        title: "Lasserie BParis SS24",
-        desc: "Lasserie BParis SS24"
-    },
-    {
-        image: "/media/img1.jpg",
-        title: "Pretty Ballerinas",
-        desc: "Lasserie BParis SS24"
-    },
+    // {
+    //     image: "/media/img3.jpg",
+    //     title: "Lasserie BParis SS24",
+    //     desc: "Lasserie BParis SS24"
+    // },
+    // {
+    //     image: "/media/img1.jpg",
+    //     title: "Pretty Ballerinas",
+    //     desc: "Lasserie BParis SS24"
+    // },
 
-    {
-        image: "/media/img2.jpg",
-        title: "Collet 23",
-        desc: "Lasserie BParis SS24"
-    },
+    // {
+    //     image: "/media/img2.jpg",
+    //     title: "Collet 23",
+    //     desc: "Lasserie BParis SS24"
+    // },
 
-    {
-        image: "/media/img3.jpg",
-        title: "Lasserie BParis SS24",
-        desc: "Lasserie BParis SS24"
-    },
+    // {
+    //     image: "/media/img3.jpg",
+    //     title: "Lasserie BParis SS24",
+    //     desc: "Lasserie BParis SS24"
+    // },
 ];
