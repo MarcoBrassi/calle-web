952c1520ccd4116bfa1472d7800a72c8e060a3c0
Lydia <aguadoortegal@digitechfp.com>
2026-05-14 14:28:25 +0200

    reestructuracion img y estructura galería 2 columnas

---
+++ src/data/gallery2col.js
@@ -0,0 +1,13 @@
+export const gallery2ColData = {
+    title: "DUST",
+    campaign: "Issue 22. COver story",
+    description: "Consultoría creativa,  Scouting,  Casting,  Producción,  Dir. Arte",
+
+    items: [
+        {
+        type: "image",
+        src: "/media/images/wbp/img1.jpg",
+        alt: "DUST - Issue 22. COver story",
+        },
+    ],
+};
